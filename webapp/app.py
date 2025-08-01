2d11b3ec26648019e0d11ee60ec5c40032d28702
Chun-Fu Chao <aaefiikmnnnr@gmail.com>
2025-08-01 20:38:23 +0800

    Jinja

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, session, render_template
+import os
+import swimclub
 
 app = Flask(__name__)
+app.secret_key = "You will never guess..."
 
 @app.get("/")
 def index():
-    return "This is a placeholder for your webapp's opening page."
+    return render_template("index.html", title = "Welcom to the Swimclub system")
+
+def populate_data():
+    if "swimmers" not in session:
+        swim_files = os.listdir(swimclub.FOLDER)
+        swim_files.remove(".DS_Store")
+        session["swimmers"] = {}
+        for file in swim_files:
+            name, *_ = swimclub.read_swim_data(file)
+            if name not in session["swimmers"]:
+                session["swimmers"][name] = []
+            session["swimmers"][name].append(file)
+
+@app.get("/swimmers")
+def display_swimmers():
+    populate_data()
+    return str(sorted(session["swimmers"]))
+
+@app.get("/files/<swimmer>")
+def get_swimemers_files(swimmer):
+    populate_data()
+    return str(session["swimmers"][swimmer])
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
